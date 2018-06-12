f3b7e3ad8594675b106f6c48dd9a4b5e75141fc6
Jesse <jesse.v.d.ent@hotmail.com>
2018-06-12 11:02:49 +0200

    Temp

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const NodeRSA = require('node-rsa');
 const key = new NodeRSA();
+const crypto = require('crypto');
 
 
 
@@ -21,22 +22,33 @@ app.use((req, res, next) => {
   // const decrypted = key.decrypt(encrypted, 'utf8');
   // console.log('decrypted: ', decrypted);
 
-  console.log("key is private: ", key.isPrivate());
-  console.log("key is public: ", key.isPublic());
+  //console.log("key is private: ", key.isPrivate());
+  //console.log("key is public: ", key.isPublic());
 
 
   const objectPublicPem = new NodeRSA(publicPem);
   const objectPrivatePem = new NodeRSA(privatePem);
-  console.log("publicPem is private: ", objectPublicPem.isPrivate());
-  console.log("publicPem is public: ", objectPublicPem.isPublic(true));
-  console.log("privatePem is private: ", objectPrivatePem.isPrivate());
-  console.log("privatePem is public: ", objectPrivatePem.isPublic(true));
-  // console.log("keypair is private: ", keypair.isPrivate());
-  // console.log("keypair is public: ", keypair.isPublic());
+  // console.log("publicPem is private: ", objectPublicPem.isPrivate());
+  // console.log("publicPem is public: ", objectPublicPem.isPublic(true));
+  // console.log("privatePem is private: ", objectPrivatePem.isPrivate());
+  // console.log("privatePem is public: ", objectPrivatePem.isPublic(true));
+  // // console.log("keypair is private: ", keypair.isPrivate());
+  // // console.log("keypair is public: ", keypair.isPublic());
 
-  console.log('privatePem: ', objectPrivatePem);
-  console.log('publicPem: ', objectPublicPem);
+  // console.log('privatePem: ', objectPrivatePem);
+  // console.log('publicPem: ', objectPublicPem);
   next();
+},
+function(objectPrivatePem, objectPublicPem, next){
+  const text = "Hello RSA!";
+  var hash = crypto.createHash('md5').update(text).digest('hex');
+  const encrypted = objectPrivatePem.encrypt(hash, text, 'base64');
+
+  //console.log(encrypted);
+  next();
+},
+function(encrypted){
+console.log(encrypted);
 });
 
 
@@ -51,5 +63,3 @@ app.get('/', function (req, res) {
 // genCertificate(clientKeypair)
 // decryptCerfiticate(clientCertificate)
 // checkHash(clientPublicKey)
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
