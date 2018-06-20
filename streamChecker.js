55bf387f89145238d07524b40f0e1c5be0fff783
Ewout <ewoutgast@gmail.com>
2018-06-20 15:06:18 +0200

    commit

---
+++ streamChecker.js
@@ -0,0 +1,26 @@
+var frames;
+
+class streamChecker{
+
+    
+    constructor(){
+        
+    }
+    newFrame(streamid, timestamp, data){
+        var newFrame = {
+            streamid: streamid,
+            timestamp: timestamp,
+            data: data
+        }
+        frames
+    }
+    getJsonData(streamid, timestamp){
+
+    }
+    checkFrames(){
+
+    }
+
+}
+
+module.exports = streamChecker;
